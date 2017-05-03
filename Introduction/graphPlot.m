481b6c8e93c3fb508863007a9c326c059cd4fd12
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-03 17:15:21 +0600

    matlab

--- Introduction/graphPlot.m
+++ Introduction/graphPlot.m
@@ -3,7 +3,7 @@ b = [1 2 3 4 5 6 7];
 
 plot (a, b);    %to see graphical view
 
-x = [1:15]; %another way declare
+x = [1:15]; %another way to declare
 y = [1:15];
 
 plot(x, y);
@@ -12,3 +12,9 @@ p = [1:0.1:3];
 q = [1:0.1:3];
 
 plot(p, q);
+
+
+n = [1:0.5:10];
+t = sin(n);
+
+plot(n, t);
