3b176ec493653b1a8fcf58d8172f93c659736948
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-03 17:41:25 +0600

    matlab

---
+++ Introduction/matrix.m
@@ -0,0 +1,11 @@
+x = [1 2 3] %row matrix
+
+y = [2; 4; 6] %cloumn matrix
+
+z = [1 2 3; 4 5 6; 7 8 9] %row-cloumn matrix
+
+p = [pi 2*pi 3*pi; 4*pi 5*pi 6*pi; 7*pi 8*pi 9*pi]; %pi row-cloumn matrix
+
+s = size(p);    %to find the size of matrix
+
+z = 2 * z;
