c6de69905696aa6e191b6e109714c8e6b1200061
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-03 16:49:26 +0600

    matlab

---
+++ Introduction/graphPlot.m
@@ -0,0 +1,14 @@
+a = [1 2 3 4 5 6 7];
+b = [1 2 3 4 5 6 7];
+
+plot (a, b);    %to see graphical view
+
+x = [1:15]; %another way declare
+y = [1:15];
+
+plot(x, y);
+
+p = [1:0.1:3];
+q = [1:0.1:3];
+
+plot(p, q);
