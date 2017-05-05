97b24200e56cc55d9e54362c30c08e92a7b2b165
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-05 13:24:30 +0600

    matlab

---
+++ Introduction/toolbox.m
@@ -0,0 +1,11 @@
+x = sqrt(9);
+
+m = sqrt([1 4; 9 16; 25 36]);   %square root of a matrix
+
+a = [1:3; 4:6; 7:9];
+
+b = sum(a); %output: add coloumn value all
+
+q = max([5 3 2 9 4 2 -1 5]);    %output: the maximum value
+
+[g, h] = max([5 3 2 9 4 2 -1 5]); %output: g->max value h->position
