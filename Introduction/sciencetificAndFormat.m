a9c0978c3ce6a530c9e3fc4d7b9dbcc118e47a0a
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-06 15:22:22 +0600

    matlab

---
+++ Introduction/sciencetificAndFormat.m
@@ -0,0 +1,12 @@
+x = 5.2*10^-9;      %scientific notation
+
+%four type: format commads: 1.format, 2.format compact, 3.format short,
+%                           4.format long
+
+y = pi;     %output: 3.1416
+
+format long
+
+z = pi;     %output: 3.141592653589793 after using format long
+
+%try to use format short e
