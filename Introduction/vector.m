0e6e1c0336bd496baa1a21bc1e2f7a1aae5cecf1
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-08 12:54:05 +0600

    matlab

---
+++ Introduction/vector.m
@@ -0,0 +1,10 @@
+a = [1 2 3 4 5];    %row vector
+b = [1 2 3 4 5]';   %column vector
+
+c = 1:0.25:2;     %row vector -increment with 0.25
+firstValueC = c(1); %first value of c
+
+zeroValue = zeros(1, 4); %four zeros value in a row
+oneValue = ones(5,1);    %five ones value in column
+
+randVector = rand(1, 5); %random value create
