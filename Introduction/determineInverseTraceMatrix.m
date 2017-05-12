93df067817ef8528d4c6155126e42848e4f8f92a
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-12 21:28:25 +0600

    matlab

---
+++ Introduction/determineInverseTraceMatrix.m
@@ -0,0 +1,10 @@
+X = [-1 5 6; 7 8 6; -5 1 2];
+
+d = det(X);
+
+I = eye(5);
+detI = det(I);      %determine of identity matrix is always 1
+
+invX = inv(X);      %inverse of matrix X
+
+traceX = trace(X);  %trace of matrix X : trace is the sum of the diagonal elements of a matrix
--- Introduction/varietyOfMatrix.m
+++ Introduction/varietyOfMatrix.m
@@ -1,3 +1,6 @@
 one = ones(4,4);        %4x4 one matrix
 zero = zeros(5, 4);     %5x4 zero matrix
 i = eye(4);             %4x4 identity matrix
+
+A = [1 2 3; 4 5 6; 7 8 9]';
+B = diag(A);            %diagonal value of matrix A
