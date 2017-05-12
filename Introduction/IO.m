a0af20d5c7990e68f47bed9e90ecb11983351aec
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-12 22:15:38 +0600

    matlab

---
+++ Introduction/IO.m
@@ -0,0 +1,7 @@
+a = input('Enter the a positive value: ');
+b = input('Enter an another positive value: ');
+c = input('Enter the last positive value: ');
+avg = (a + b + c) / 3;
+
+disp('Hello Mrinmoi');
+disp('Your Average value is: ');
---
+++ Introduction/geometry.m
@@ -0,0 +1,3 @@
+p = pi / 4;     %here pi = 45degree
+s = sin(p);
+c = cos(p);
