a10cab5284d8465be8c934cc70507a800c571ef5
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-18 00:08:54 +0600

    matlab

---
+++ Introduction/plot3D.m
@@ -0,0 +1,10 @@
+%x = sin(t), y = cos(t), z = t
+
+t = [0:pi / 50:10*pi];
+plot3(t, cos(t), sin(t));
+xlabel('cos(t) value');
+ylabel('sin(t) value');
+zlabel('t value');
+title('3D plot');
+
+legend('3D Graph');
