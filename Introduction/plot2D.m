fd62e3008a3dcbb586c8c78a0972418774731a97
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-17 23:58:43 +0600

    matlab

---
+++ Introduction/plot2D.m
@@ -0,0 +1,8 @@
+x = [1 2 3 4 5];
+y = [1 4 9 16 25];
+
+%plot(x, y);
+plot(x, y, 'g --');     %change color: green and line style: dashed
+xlabel('x value');
+ylabel('y value');
+title('According to the data');
