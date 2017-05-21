5e5c57b78a8cc56bd7620d0ef191a58695a0e293
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-21 20:04:15 +0600

    matlab

---
+++ Introduction/mathFunction.m
@@ -0,0 +1,20 @@
+%factorial
+x = factorial(9);
+disp(x);
+
+%Sqaure root
+y = sqrt(25);
+disp(y);
+
+%nSqaure Root
+z = nthroot(64, 3);
+disp(z);
+
+%Trigonometric Functions
+
+%Sin value
+a = sin(0);
+b = sin(pi / 2);
+%Cos value
+c = cos(0);
+d = cos(90);
--- Introduction/plot3D.m
+++ Introduction/plot3D.m
@@ -1,7 +1,8 @@
 %x = sin(t), y = cos(t), z = t
 
-t = [0:pi / 50:10*pi];
+t = 0:pi / 50:10*pi;
 plot3(t, cos(t), sin(t));
+grid on;                        %to show gird on axis
 xlabel('cos(t) value');
 ylabel('sin(t) value');
 zlabel('t value');
