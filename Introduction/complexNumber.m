2c92b018732856e885ffe19fe033ff3c5e66de46
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-24 09:39:15 +0600

    matlab

---
+++ Introduction/complexNumber.m
@@ -0,0 +1,13 @@
+X = 2 + 3 * 1i;      %in Matlab complex number wil be "i or j"
+Y = 2 + 3 * 1i;      %both output is: 2.0000 + 3.0000i
+
+%make complex number
+a = 2;
+b = 3;
+Z = complex(a, b);
+
+%calculations :-
+p = X +Y;
+q = X - Y;
+r = X * Y;
+s = X / Y;
