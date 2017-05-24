c9f56e9c6171ab03c223012ac2c935565ca18df9
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-24 09:59:10 +0600

    matlab

--- Introduction/complexNumber.m
+++ Introduction/complexNumber.m
@@ -17,3 +17,4 @@ xx = real(X);
 xy = imag(X);
 xz = abs(X);        %abs(X) = sqrt(real^2 + imag^2)
 xa = angle(X);      %angle(X) = tan^-1(imag/real)
+xb = conj(X);       %conj shift the complex number in 180 degree
